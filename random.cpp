f6fc2802ddd4047623b733c7da34e77365f02578
Alex <kuepper.alexander@web.de>
2017-11-06 15:42:37 +0100

    first commit

---
+++ random.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+int main() {
+	srand(time(NULL));
+	int random = rand()%100;
+	int number;
+	int counter = 0;
+	cout << "Erraten Sie die Zahl: " << endl;
+		while(random != number) {
+		cin >> number;
+
+		
+		if(random < number) {
+			cout << "Die Zahl ist zu gross" << endl;
+		}
+		if(random > number) {
+			cout << "Die Zahl ist zu klein" << endl;
+		}
+		counter++;
+	}
+	cout << "Das ist die richtige Zahl" << endl;
+	cout << "Versuchsanzahl: " << counter << endl;
+	while(true) {}
+	return 0;
+	
+}
